7ca057c8206efcdd192e00805b8bb2c858b7ca7e
Akshay kumar <80337826+akshay1roy@users.noreply.github.com>
2025-02-04 02:47:33 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 4000;
 
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://email-verify-frontend-41j5.onrender.com']
 
 app.use(express.json());
 app.use(cookieParser());
