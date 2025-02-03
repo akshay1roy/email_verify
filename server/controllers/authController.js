9a94f1665d8c0d25ab1e0c0075f13c2380f364eb
Akshay kumar <80337826+akshay1roy@users.noreply.github.com>
2025-02-04 03:11:49 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,7 +31,7 @@ export const register = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_EVN === 'production',
+            secure: true ||  process.env.NODE_EVN === 'production',
             sameSite: process.env.NODE_EVN === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
@@ -94,7 +94,7 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_EVN === 'production',
+            secure: true || process.env.NODE_EVN === 'production',
             sameSite: process.env.NODE_EVN === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
